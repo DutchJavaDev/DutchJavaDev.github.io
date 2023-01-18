d782eb91d31bb563db7efdbd3183bd0be2e1f490
Boris <Borismulder@outlook.com>
2023-01-18 18:25:26 +0100

    eyyy f you github

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 document.addEventListener("DOMContentLoaded",async function(){
-    await fetch('https://api.db-ip.com/v2/free/self', function(data) {
-        console.log(JSON.stringify(data, null, 2));
-      });
+   
 });
